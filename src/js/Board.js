e110b38762f5b9c081329fca7f53338a8303c59e
Greg-PC <grets.gergo@gmail.com>
2022-03-28 21:25:37 +0200

    added fetch and board state

--- src/js/Board.js
+++ src/js/Board.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import { Row } from 'react-bootstrap';
 import {BoardNoteTable} from './BoardNoteTable';
-import { PopupModal } from './PopupModal';
 
 
 export class Board extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            allNotes: {
+                toDoBoard: {
+                    title: "ToDo",
+                    notes: []
+                },
+                inProgressBoard: {
+                    title: "In Progress",
+                    notes: []
+                },
+                doneBoard: {
+                    title: "Done",
+                    notes: []
+                },
+                blockedBoard: {
+                    title: "Blocked",
+                    notes: []
+                },
+            }
+        }
+    }
 
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then((users) => this.setState({notes: users}));
+    }
 
     render(){
         return(
@@ -13,10 +41,10 @@ export class Board extends React.Component{
                 <h1>Project Board</h1>
                 <hr/>
                 <Row md={5}>
-                    <BoardNoteTable name="TODO"/>
-                    <BoardNoteTable name="In Progress"/>
-                    <BoardNoteTable name="Done"/>
-                    <BoardNoteTable name="Blocked"/>                    
+                    <BoardNoteTable name={this.state.allNotes.toDoBoard.title} tNotes={this.state.allNotes.toDoBoard.notes}/>
+                    <BoardNoteTable name={this.state.allNotes.inProgressBoard.title} iPNotes={this.state.allNotes.inProgressBoard.notes}/>
+                    <BoardNoteTable name={this.state.allNotes.doneBoard.title} dNotes={this.state.allNotes.doneBoard.notes}/>
+                    <BoardNoteTable name={this.state.allNotes.blockedBoard.title} bNotes={this.state.allNotes.blockedBoard.notes}/>                    
                 </Row>
             </div>
         )
