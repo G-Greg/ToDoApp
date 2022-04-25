fe7178aa42fcaf1880fc0b03ae21b93eace009d0
Gréts Gergő <grets.gergo@gmail.com>
2022-04-25 20:13:08 +0200

    added new items

---
+++ ToDoReactWithBackend/react-backend/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace react_backend.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public int Priority { get; set; }
+        public string? CardTitle { get; set; }
+        public string? Description { get; set; }
+        public string? Date { get; set; }
+
+    }
+}
